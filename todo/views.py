cd7064736aaeb43e7cbb0b65bfb259c6d0cccf75
1f10220225 <s1f102202257@iniad.org>
2023-07-06 12:26:44 +0900

    PEP8に従うように修正

--- todo/views.py
+++ todo/views.py
@@ -21,6 +21,7 @@ def index(request):
     }
     return render(request, 'todo/index.html', context)
 
+
 def detail(request, task_id):
     try:
         task = Task.objects.get(pk=task_id)
@@ -30,4 +31,4 @@ def detail(request, task_id):
     context = {
         'task': task,
     }
-    return render (request, 'todo/detail.html', context)
+    return render(request, 'todo/detail.html', context)
